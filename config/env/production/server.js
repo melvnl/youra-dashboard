56fbf6cb7dac7335dc7cd7d1040427c8b7ca4168
melvnl <melvinlim2000@gmail.com>
2022-08-04 13:45:09 +0700

    update: db config

---
+++ config/env/production/server.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false,
+  },
+});
